7c983e0d326f0254f48541722243af044cfba7ad
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-02-17 17:16:06 +0100

    removing unnecessary code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,6 @@
 package org.example;
 
-import exceptions.InvalidArmorException;
-import exceptions.InvalidWeaponException;
-
 public class Main {
     public static void main(String[] args) {
-        Mage testMage = new Mage("Judy");
-        Armor testShorts = new Armor("Basic shorts", 1, Item.Slot.legs, Armor.ArmorType.cloth,0,0,1);
-        Armor testHat = new Armor("Basic hat", 1, Item.Slot.head, Armor.ArmorType.cloth, 0,0,1);
-        Armor testShirt = new Armor("Basic shirt", 1, Item.Slot.body, Armor.ArmorType.cloth,0,0,1);
-        Weapon testWeapon = new Weapon("Basic wand", 1, Item.Slot.weapon, Weapon.WeaponType.wand, 1);
-        testMage.levelUp();
-        try {
-            testMage.equipArmor(testShorts);
-        } catch (InvalidArmorException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            testMage.equipArmor(testHat);
-        } catch (InvalidArmorException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            testMage.equipArmor(testShirt);
-        } catch (InvalidArmorException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            testMage.equipWeapon(testWeapon);
-        } catch (InvalidWeaponException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(testMage.display());
     }
 }
